c6e2f6063b06e0a54030e74e280c5fe34baf0b7d
Mrinal Tyagi <mrinaltyagi1912@gmail.com>
2020-12-09 16:53:03 +0000

    new file : added_quiz.js

---
+++ quiz.js
@@ -0,0 +1,5 @@
+var readlineSync = require('readline-sync');
+
+var score = 0;
+var userName = readlineSync.question("Please enter your name: ");
+console.log("Welcome " + userName + "! Dive into the game");
