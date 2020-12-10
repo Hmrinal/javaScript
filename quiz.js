966959b0c1171bd59ce9577655ad9e54f184d57c
Mrinal Tyagi <mrinaltyagi1912@gmail.com>
2020-12-10 17:45:50 +0000

    fix: fixed script error

--- quiz.js
+++ quiz.js
@@ -1,5 +1,5 @@
 var readlineSync = require('readline-sync');
 
 var score = 0;
-var userName = readlineSync.question("Please enter your name: ");
-console.log("Welcome " + userName + "! Dive into the game");
+var userName = readlineSync.question("Please enter your name:  ");
+console.log("Welcome "+ userName + "! Dive into the game for this fun quiz");
