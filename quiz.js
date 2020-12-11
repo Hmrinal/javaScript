dac08c9c0356a8f1c125d9960e68ba0350b8e5d4
Mrinal Tyagi <mrinaltyagi1912@gmail.com>
2020-12-11 16:49:43 +0000

    working on question_bank

--- quiz.js
+++ quiz.js
@@ -26,9 +26,39 @@ var questions=[{
   ` ,
   answer:"c"
 },{
-  question:"What can be his Costume Color? ",
-  answer:"blue"
-}];
+  question:` Which network airs 'Suits?'
+  a: USA
+  b: NBC
+  c: CBS
+  d: TNT
+  ` ,
+  answer:"a"
+},{
+  question:` Who is the female shark in charge of the 'Suits' law firm?
+  a: Rachel Zane
+  b: Donna Paulsen
+  c: Katrina Bennett
+  d: Jessica Pearson
+  ` ,
+  answer:"d"
+},{
+  question:` The firm hires law graduates from ________?
+  a: Yale
+  b: Columbia
+  c: Harvard
+  d: Stanford
+  ` ,
+  answer:"c"
+},{
+  question:` _____ is the firm's expert on all financial matters?
+  a: Charles Forstman
+  b: Robert Zane
+  c: Jeff Malone
+  d: Louis Litt
+  ` ,
+  answer:"d"
+}
+];
 
 for(var i=0; i < questions.length; i++){
   var currentQuestion = questions[i];
